2117eba433f36e7b3b90bc26816f266dc3eb465d
Martin Koller <kollermartin00@gmail.com>
2021-04-17 12:29:27 +0200

    added languages and redirect

--- js/script.js
+++ js/script.js
@@ -41,4 +41,7 @@ navContact.addEventListener("click",function(){
     scrollToElement(contactEl);
 });
 
+function pageRedirect() {
+    window.location.replace("http://127.0.0.1:5500/bottle.html");
+} 
 
